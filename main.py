4285de1ab069e929c1b211ba0624ca5f8e7c8d4f
Mohammad Aadil <46608674+moaadil@users.noreply.github.com>
2019-06-23 13:00:24 -0700

    Code added

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+from bs4 import BeautifulSoup as BS        
+import requests                             
+
+keyword = input("\nEnter the keyword: ")   #keyword to search for
+img_url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=isch"    #search query with keyword
+headers={'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0"}   #User agent header
+site_html = requests.get(img_url, headers).text         #raw html data of the search results page
+soup = BS(site_html, "html.parser")         #creating a BeautifulSoup object using the raw data and a html parser
+
+#This code will download the first thumbnail sized photo that it encounters
+for element in soup.findAll('img'):
+    
+    url = element.get("src")            #gets the src link of image
+    r = requests.get(url)               #gets the url data
+
+    with open("./img.png", "wb") as photo:      #writes the url data to a file
+                photo.write(r.content)
+                break
+    break                                       
+#as google doesnt allow webscraping on its search results, we will use an application called google_images_download
+from google_images_download import google_images_download
+response = google_images_download.googleimagesdownload()    #creating an object
+arguments = {"keywords": keyword, "format": "jpg", "limit":1, "print_urls":True, "size": "medium"}      #function parameters
+response.download(arguments)            #downloading the image
